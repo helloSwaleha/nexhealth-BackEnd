ca4e879b408f417ad14e053d70ee8683bacd0c90
Swaleha <swalehapatwekar16@gmail.com>
2026-04-06 22:05:56 +0530

    Update PrescriptionRepository.java

--- src/main/java/com/clinic/management/repository/PrescriptionRepository.java
+++ src/main/java/com/clinic/management/repository/PrescriptionRepository.java
@@ -16,7 +16,6 @@ public interface PrescriptionRepository extends JpaRepository<Prescription, Long
 
     List<Prescription> findByAppointmentId(Long appointmentId);
 
-    List<Prescription> findByPatientId(Long patientId);
 
     // FIXED: Removed the @EntityGraph findAll() that was causing the error.
     // JpaRepository already provides a standard findAll().
